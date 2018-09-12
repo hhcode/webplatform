31dd255d3a08ba82d2a86e333bb52d3eba193911
huangjihui <huangjihui@enmonster.com>
2018-09-12 19:15:39 +0800

    @Value

--- src/main/java/com/huang/entity/UcpaasSmsSendRequest.java
+++ src/main/java/com/huang/entity/UcpaasSmsSendRequest.java
@@ -1,9 +1,9 @@
 package com.huang.entity;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import lombok.ToString;
 import org.apache.commons.codec.digest.DigestUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -13,7 +13,6 @@ import org.springframework.stereotype.Component;
  * @Author huangjihui
  * @Date 2018/9/12 10:10
  */
-@Data
 @ToString
 @Component
 public class UcpaasSmsSendRequest {
@@ -21,46 +20,77 @@ public class UcpaasSmsSendRequest {
     /**
      * 云之讯平台keyCode
      */
-    @Value("${sms.ucpass.keyCode}")
+
+    @Getter
     private String keyCode;
+
+    @Value("${sms.ucpass.keyCode}")
+    public void setKeyCode(String keyCode) {
+        this.keyCode = keyCode;
+    }
 
     /**
      * 在云之讯平台用户名
      */
-    @Value("${sms.ucpass.userName}")
+
+    @Getter
     private String userName;
+
+    @Value("${sms.ucpass.userName}")
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
 
     /**
      * 回调url
      */
-    @Value("${sms.ucpass.backUrl}")
+
+    @Getter
     private String backUrl;
+
+    @Value("${sms.ucpass.backUrl}")
+    public void setBackUrl(String backUrl) {
+        this.backUrl = backUrl;
+    }
 
     /**
      * 请求uri
      */
-    @Value("${sms.ucpass.serviceUri}")
+    @Getter
     private String serviceUri;
+
+    @Value("${sms.ucpass.serviceUri}")
+    public void setServiceUri(String serviceUri) {
+        this.serviceUri = serviceUri;
+    }
 
     /**
      * 时间戳
      */
+    @Setter
+    @Getter
     private Long timeStamp;
 
 
     /**
      * iccid
      */
+    @Setter
+    @Getter
     private String iccid;
 
     /**
      * 信息内容
      */
+    @Setter
+    @Getter
     private String content;
 
     /**
      * 唯一识别码
      */
+    @Setter
+    @Getter
     private Integer msgId;
 
     public static String getUriAndParam(UcpaasSmsSendRequest request) {
